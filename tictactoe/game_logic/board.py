f30818461d681b5c5134592b56071b9a0a8750b8
jose-arce06 <a224200192@unison.mx>
2026-01-20 19:40:37 -0700

    55

---
+++ tictactoe/game_logic/board.py
@@ -0,0 +1,12 @@
+"""
+Docstring for tictactoe.game_logic.board
+Author: jose arce
+"""
+
+def display_board(dboard:dict): None:
+    """
+    Docstring for display_board
+    """
+print(f"{d[0]}|{d[1]}|{d[3]}")
+
+
